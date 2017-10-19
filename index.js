5237a7210d24de503ebff6ebdd81f471b6f98936
Hanna Yanglou <yanglou@ualberta.ca>
2017-10-19 16:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
 
-  if (string === lowercase){
+  if (string === string.toLowerCase()){
     return "I can\'t hear you!"
   }
-  else if (string === uppercase){
+  else if (string === string.toCpperCase()){
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
